dfd47219bf60745a074c092ef43339e945b1096d
trunggdungg <le25011985@gmail.com>
2023-12-29 20:01:32 +0700

    project_convert

--- Project_convert/assets/js/main.js
+++ Project_convert/assets/js/main.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,/*thanh dieu huong*/
+    dots:true,
+    autoplay: true, /* Auto-play option */
+    autoplayTimeout: 3000, 
+    responsive:{
+        0:{
+            items:1
+        },  
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
   });
+
